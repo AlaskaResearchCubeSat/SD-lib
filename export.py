2cd1b3e0a7c0938dc38f2345d216c3facdc3643a
Jesse Frey <jmfrey@alaska.edu>
2015-06-09 13:41:09 -0800

    Updated export script

--- export.py
+++ export.py
@@ -2,6 +2,9 @@
 
 import shutil
 import os
+import subprocess
+import sys
+import re
 
 inputDir="./"
 prefix="Z:\Software"
@@ -9,12 +12,45 @@ lib=os.path.join(prefix,"lib")
 include=os.path.join(prefix,"include")
 basename="SD-lib"
 
-for folder in ("UCA1 Release","UCA1 Debug","UCB1 Release","UCB1 Debug"):
-    outname=basename+"_"+"_".join(folder.split())+".hza"
-    outpath=os.path.join(lib,outname)
-    inpath=os.path.join(inputDir,os.path.join(folder,basename+".hza"))
-    print("Copying "+inpath+" to "+outpath)
-    shutil.copyfile(inpath,outpath)
+#find which crossbuild to use
+rowleyPath="C:\\Program Files (x86)\\Rowley Associates Limited\\"
+#list rowley folder in program files
+dirs=os.listdir(rowleyPath);
+#initialize variables
+path=None
+version=None
+#search for MSP430 crossworks
+for folder in dirs:
+	m=re.search("CrossWorks for MSP430 ([0-9\\.])",folder)
+	if m is not None:
+		#get version tuple
+		ver=tuple(map(int,m.group(1)[0].split('.')))
+		#check if a version was found
+		if version is None or ver>version:
+			version=ver
+			path=folder
+
+#get bath to crossbuild
+crossbuild=os.path.join(rowleyPath,path,'bin','crossbuild.exe')
+
+for config in ("UCA1 Release","UCA1 Debug",
+			   "UCA2 Release","UCA2 Debug",
+			   "UCA3 Release","UCA3 Debug",
+			   "UCB1 Release","UCB1 Debug"):
+    
+	#build using crossbuild
+	print("Building "+config);
+	rc=subprocess.call([crossbuild,'-config',config,basename+'.hzp'])
+	#check return code
+	if rc!=0:
+		print("Error : project did not build exiting")
+		exit(rc)
+
+	outname=basename+"_"+"_".join(config.split())+".hza"
+	outpath=os.path.join(lib,outname)
+	inpath=os.path.join(inputDir,os.path.join(basename+" "+config,basename+".hza"))
+	print("Copying "+inpath+" to "+outpath)
+	shutil.copyfile(inpath,outpath)
 
 for file in ("SDlib.h",):
     outpath=os.path.join(include,file)
