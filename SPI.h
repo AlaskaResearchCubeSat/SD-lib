68b19f9f38c8d2cb8e41b6ba48b3a71a8bcc855e
Jesse Frey <jmfrey@alaska.edu>
2016-01-05 16:55:51 -0900

    Change spiReadFrame and spiSendFrame to return int
    This fixes the problem of incorrect error codes returned from these
    functions

--- SPI.h
+++ SPI.h
@@ -20,8 +20,8 @@ void SPISetup (void);
 void SPI_fast(void);
 void SPI_slow(void);
 unsigned char spiSendByte(const unsigned char data);
-unsigned char spiReadFrame(unsigned char* pBuffer, unsigned int size);
-unsigned char spiSendFrame(const unsigned char* pBuffer, unsigned int size);
+int spiReadFrame(unsigned char* pBuffer, unsigned int size);
+int spiSendFrame(const unsigned char* pBuffer, unsigned int size);
 void spiDummyClk(void);
 void SPIShutdown(void);
 
